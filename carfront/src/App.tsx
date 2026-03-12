9dfca2b9625e32b7f58344e0df76b9729140f711
rjsdyd <rjsdyd0113@naver.com>
2026-03-12 17:44:25 +0900

    feat: 20260312

---
+++ carfront/src/App.tsx
@@ -0,0 +1,25 @@
+import { Container, AppBar, Toolbar, Typography, CssBaseline } from "@mui/material"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import Carlist from "./components/Carlist";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <Container maxWidth='xl'>
+    <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Car Shop
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <QueryClientProvider client={queryClient}>
+        <Carlist></Carlist>
+      </QueryClientProvider>
+    </Container>
+  )
+}
+
+export default App
---
+++ carfront/src/types.ts
@@ -0,0 +1,17 @@
+export type CarResponse = {
+  brand: string;
+  model: string;
+  color: string;
+  registrationNumber: string;
+  modelYear: number;
+  price: number;
+  _links: {
+    self: {
+      href: string;
+    }, car: {
+      href: string;
+    }, owner: {
+      href: string;
+    }
+  }
+}
